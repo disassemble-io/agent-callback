3e89d68abb063d49bc7ba51b7fb7416ce9233d81
Tyler Sedlar <sedlarizona@gmail.com>
2016-01-23 17:48:13 -0700

    Add JarCaller example

---
+++ src/test/java/JarCaller.java
@@ -0,0 +1,44 @@
+import me.sedlar.agent.CallbackAgent;
+import me.sedlar.agent.callback.CallFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
+/**
+ * @author Tyler Sedlar
+ * @since 1/23/16
+ */
+public class JarCaller {
+
+    private static void executeJar(String jarPath, String[] args) throws Exception {
+        JarFile jar = new JarFile(jarPath);
+        String main = jar.getManifest().getMainAttributes().getValue("Main-Class");
+        if (main != null) {
+            URLClassLoader loader = new URLClassLoader(new URL[] {new File(jarPath).toURI().toURL()});
+            Class<?> mainClass = loader.loadClass(main);
+            Method mainMethod = mainClass.getMethod("main", String[].class);
+            mainMethod.invoke(null, new Object[] {args});
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new CallbackAgent(
+                cn -> cn.name.contains("Boot"),
+                mn -> mn.name.equals("method")
+        ).attach();
+        CallFactory.setCallDelay(2000L);
+        CallFactory.setInvokePredicate(callback -> {
+            String methodKey = callback.methodKey;
+            Object[] methodArgs = callback.args;
+            System.out.println("call: " + methodKey + " @ " + Arrays.deepToString(methodArgs));
+            return true;
+        });
+        executeJar("./src/test/Boot.jar", args);
+    }
+}
